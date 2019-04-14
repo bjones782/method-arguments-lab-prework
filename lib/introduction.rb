86d25d93882366732a0093f296e3d393c07a297f
Brooke Jones <bjones782@gmail.com>
2019-04-14 16:59:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,3 @@
-def introduction_with_language(name)
-  puts "Hi, my name is #{name}."
-end
-
-introduction_with_language("Josh")
-
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
